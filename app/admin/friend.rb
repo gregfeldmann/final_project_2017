dcdd8d1851d84c92a2e66eaa40642aa350d9007a
Raghu Betina <raghu@firstdraft.com>
2017-08-10 01:37:44 +0000

    Whitelist columns in Friend for ActiveAdmin

--- app/admin/friend.rb
+++ app/admin/friend.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Friend do
 
+ permit_params :sender_id, :accepted_at
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
